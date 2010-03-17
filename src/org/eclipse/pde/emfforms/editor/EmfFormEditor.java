651fbfdd054a29062305e269763200ae57ae43c8
bcabe <bcabe>
2010-03-17 10:44:45 +0000

    extracting SharedClipboardAdapterFactoryEditingDomain

--- src/org/eclipse/pde/emfforms/editor/EmfFormEditor.java
+++ src/org/eclipse/pde/emfforms/editor/EmfFormEditor.java
@@ -8,9 +8,11 @@
  * Contributors:
  *     Anyware Technologies - initial API and implementation
  *
- * $Id: EmfFormEditor.java,v 1.36 2010/01/04 13:27:36 bcabe Exp $
+ * $Id: EmfFormEditor.java,v 1.37 2010/03/17 10:44:45 bcabe Exp $
  */
 package org.eclipse.pde.emfforms.editor;
+
+import org.eclipse.pde.emfforms.internal.editor.SharedClipboardAdapterFactoryEditingDomain;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -550,30 +552,6 @@ public abstract class EmfFormEditor<O extends EObject> extends FormEditor implem
 					}
 				}
 			}
-		}
-	}
-
-	private static class SharedClipboardAdapterFactoryEditingDomain extends AdapterFactoryEditingDomain {
-		private static Collection<Object> clipboard;
-
-		/**
-		 * @param adapterFactory
-		 * @param commandStack
-		 * @param resourceToReadOnlyMap
-		 */
-		public SharedClipboardAdapterFactoryEditingDomain(AdapterFactory adapterFactory, CommandStack commandStack, Map<Resource, Boolean> resourceToReadOnlyMap) {
-			super(adapterFactory, commandStack, resourceToReadOnlyMap);
-
-		}
-
-		@Override
-		public Collection<Object> getClipboard() {
-			return SharedClipboardAdapterFactoryEditingDomain.clipboard;
-		}
-
-		@Override
-		public void setClipboard(Collection<Object> clipboard) {
-			SharedClipboardAdapterFactoryEditingDomain.clipboard = clipboard;
 		}
 	}
 
